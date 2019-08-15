fd2719b536f3126d0c3300fa5344d454fc867171
anton.g@labelplatform.com <anton.g@labelplatform.com>
2019-08-15 18:58:21 +0300

    cardBinListCross
    Signed-off-by: anton.g@labelplatform.com <anton.g@labelplatform.com>

--- BinMapping_checker_selenium.py
+++ BinMapping_checker_selenium.py
@@ -9,6 +9,7 @@ import datetime
 from vertica_python import errors
 
 import config
+from config import konnektiveAdminPannelURL
 import logging
 import csv
 from selenium import webdriver
@@ -21,48 +22,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import core_functions
+
 start = timer()
+
+
 # TODO: в корфункшинс вынгести функцию проверки наличия БИНа в бинмаппинге
-# TODO: в корфункшинс вынгести функцию выборку SQL из БД клиентов, котоые будут чарджиться завтра
 # TODO: в корфункшинс вынгести функцию проверки БИНа в бинмаппинге
 # TODO: в корфункшинс вынгести функцию Импорт Ордер
 # TODO: в корфункшинс вынгести функцию Рефанда транзакции
 # TODO: провести проверку, если БИН, на котором прошла последняя транза не из списка, вызвать ф-ю Рефанда транзакции
 # TODO: отправка итогового репорта в телеграм чат
-
-# fp = core_functions.browser_init()
-# browser = core_functions.browser_open(fp)
-# core_functions.login_Konnektive(browser, config.seleniumUserName, config.seleniumPassword)
-#
-#
-#
-#
-#
-#
-# brows_URL = 'https://crm.konnektive.com/merchants/binmapping/'
-# browser.get(brows_URL)
-# core_functions.wait_until_element_visible(browser, By.CSS_SELECTOR, '#-row-1 > td:nth-child(1)')
-#
-# RowsCount = len(browser.find_elements_by_tag_name('tr'))
-#
-# binList = []
-# for counter in range(RowsCount-1):
-#     CyclePathStartRange = f'#-row-{str(counter + 1)} > td:nth-child(3)'
-#     StartRangeValue = browser.find_element_by_css_selector(CyclePathStartRange).text
-#     binList.append(StartRangeValue)
-
-
-# cardBinListCross = StartRangeValue in BinList
-# if cardBinListCross:
-#     CyclePathEditButton = '#-row-' + str(counter + 1) + ' > td:nth-child(8) > span:nth-child(1)'
-#     browser.find_element_by_css_selector(CyclePathEditButton).click()
-#     core_functions.wait_until_element_visible(
-#         By.CSS_SELECTOR, '.checkbox > label:nth-child(1) > input:nth-child(1')
-#     browser.find_element_by_css_selector('.checkbox > label:nth-child(1) > input:nth-child(1)').click()
-#     browser.find_element_by_css_selector('input.btn').click()
-#     sleep(30)
-
-
 
 def list_costumers_from_DB():
 	this_function_name = sys._getframe().f_code.co_name
@@ -94,11 +63,10 @@ def list_costumers_from_DB():
 			FROM konn.bill_info AS bi
 			WHERE bi.konn_nextBillDate= :propA
 			AND (bi.konn_status='ACTIVE' OR bi.konn_status='TRIAL' OR bi.konn_status='RECYCLE_BILLING')
-			AND bi.konn_merchant LIKE '%Check%';"""
+			AND bi.konn_merchant LIKE '%Check%' LIMIT 20;"""
 			cur.execute(SQLRequest, {'propA': nextBillDate})
 			for row in cur.iterate():
 				resultSQLList.append(row)
-			print(SQLRequest)
 			cur.close()
 			connection.close()
 	except socket.error as socketerror:
@@ -140,12 +108,35 @@ def list_costumers_from_DB():
 
 resultFilteredCostumerList = list_costumers_from_DB()
 
+fp = core_functions.browser_init()
+browser = core_functions.browser_open(fp)
+core_functions.login_Konnektive(browser, config.seleniumUserName, config.seleniumPassword)
 
+brows_URL = f'{konnektiveAdminPannelURL}merchants/binmapping/'
+browser.get(brows_URL)
+core_functions.wait_until_element_visible(browser, By.CSS_SELECTOR, '#-row-1 > td:nth-child(1)')
 
+RowsCount = len(browser.find_elements_by_tag_name('tr'))
+binList = []
 
+for counter in range(RowsCount - 1):
+	CyclePathStartRange = f'#-row-{str(counter + 1)} > td:nth-child(3)'
+	StartRangeValue = browser.find_element_by_css_selector(CyclePathStartRange).text
+	binList.append(StartRangeValue)
+print(binList)
 
+print(len(resultFilteredCostumerList))
 
-
+for resultFilteredList in range(len(resultFilteredCostumerList) - 1):
+	cardBinListCross = StartRangeValue in binList
+# if cardBinListCross:
+#     CyclePathEditButton = '#-row-' + str(counter + 1) + ' > td:nth-child(8) > span:nth-child(1)'
+#     browser.find_element_by_css_selector(CyclePathEditButton).click()
+#     core_functions.wait_until_element_visible(
+#         By.CSS_SELECTOR, '.checkbox > label:nth-child(1) > input:nth-child(1')
+#     browser.find_element_by_css_selector('.checkbox > label:nth-child(1) > input:nth-child(1)').click()
+#     browser.find_element_by_css_selector('input.btn').click()
+#     sleep(30)
 
 
 end = timer()
