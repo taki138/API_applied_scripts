2b2791214eaa0e1862fd772b0d633a9d1c989df0
anton.g@labelplatform.com <anton.g@labelplatform.com>
2019-08-28 18:47:09 +0300

    checking for refund
    Signed-off-by: anton.g@labelplatform.com <anton.g@labelplatform.com>

--- BinMapping_checker_selenium.py
+++ BinMapping_checker_selenium.py
@@ -30,6 +30,7 @@ start = timer()
 PARAMS = {
 	'loginId': config.loginId,
 	'password': config.password,
+	'sortDir': '0',
 	}
 
 outputFilename = f'verification_charge_results_{datetime.date.today().strftime("%d.%m.%Y")}.csv'
@@ -39,10 +40,9 @@ core_functions.check_directory_existence(outputFilePath)
 
 # TODO: в корфункшинс вынгести функцию проверки наличия БИНа в бинмаппинге
 # TODO: в корфункшинс вынгести функцию проверки БИНа в бинмаппинге
-# TODO: в корфункшинс вынгести функцию Импорт Ордер
 # TODO: в корфункшинс вынгести функцию Рефанда транзакции
 # TODO: провести проверку, если БИН, на котором прошла последняя транза не из списка, вызвать ф-ю Рефанда транзакции
-# TODO: отправка итогового репорта в телеграм чат
+
 
 def list_costumers_from_DB():
 	this_function_name = sys._getframe().f_code.co_name
@@ -141,21 +141,29 @@ for counter in range(RowsCount - 1):
 	StartRangeValue = browser.find_element_by_css_selector(CyclePathStartRange).text
 	binList.append(StartRangeValue)
 
+#
+# for i in range(len(resultFilteredList)):
+# 	if resultFilteredList[i][1] in binList:
+# 		writedWalues = 'customerId', str(resultFilteredList[i][0]), 'Have Binmapping'
+# 		print(writedWalues)
+# 	else:
+# 		customerId = resultFilteredList[i][0]
+# 		PARAMS['customerId'] = customerId
+# 		result = core_functions.konnektiveTransactionsQuery(PARAMS)[1]
+# 		# result = core_functions.konnektiveImportOrder(PARAMS)[1]['result']
+# 		writedWalues = 'customerId', str(customerId), result['result']
+# 		print(writedWalues)
+# 	core_functions.csv_result_writer(outputFilename, outputFilePath, writedWalues)
 
-for i in range(len(resultFilteredList)):
-	if resultFilteredList[i][1] in binList:
-		writedWalues = 'customerId', str(resultFilteredList[i][0]), 'Have Binmapping'
-		print(writedWalues)
-	else:
-		customerId = resultFilteredList[i][0]
-		PARAMS['customerId'] = customerId
-		result = core_functions.konnektiveTransactionsQuery(PARAMS)[1]
-		# result = core_functions.konnektiveImportOrder(PARAMS)[1]['result']
-		writedWalues = 'customerId', str(customerId), result['result']
-		print(writedWalues)
-	core_functions.csv_result_writer(outputFilename, outputFilePath, writedWalues)
+# core_functions.send_file_by_bot(outputFilePath, outputFilename)
 
 #############################################################
+for i in range(len(resultFilteredList)):
+	customerId = resultFilteredList[i][0]
+	PARAMS['customerId'] = customerId
+	merchant = core_functions.konnektiveTransactionsQuery(PARAMS)[1]['message']['data'][0]['merchant']
+	if "Checkout" not in merchant:
+		pass
 
 
 end = timer()
